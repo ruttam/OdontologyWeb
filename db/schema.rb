052d841794a15efb32266dab5a6f86eae3cc51c1
ruttam <ruta.tamsauskaite@gmail.com>
2015-05-27 17:23:02 +0300

    Admin + User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527114902) do
+ActiveRecord::Schema.define(version: 20150527140836) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -45,5 +45,19 @@ ActiveRecord::Schema.define(version: 20150527114902) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "surname"
+    t.string   "personalCode"
+    t.string   "telephoneNr"
+    t.string   "email"
+    t.string   "address"
+    t.string   "comments"
+    t.boolean  "isActive"
+    t.datetime "lastAppointment"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
